bcf0bc8e3d0c51e2cee97e42e1ad6004d4a926fc
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-05-20 09:27:53 +0530

    added

--- vite-project/src/components/Square.jsx
+++ vite-project/src/components/Square.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 
-const Square = () => {
+const Square = ({value}) => {
+    return <button>{value}</button>
+}
+
+const Board = () => {
     return (
         <main className=''>
             <section className=''>
-                <button>1</button>
-                <button>2</button>
-                <button className='square'>3</button>
+                <Square value={1} />
+                <Square value={2}/>
+                <Square value={3}/>
 
             </section>
             <section>
-                <button className='square'>4</button>
-                <button className='square'>5</button>
-                <button className='square'>6</button>
+                <Square value={4} />
+                <Square value={5} />
+                <Square value={6} />
 
             </section>
             <section>
-                <button className='square'>7</button>
-                <button className='square'>8</button>
-                <button className='square'>9</button>
+                <Square value={7} />
+                <Square value={8} />
+                <Square value={9} />
 
             </section>
         </main>
     )
 }
 
-export default Square
+export default Board

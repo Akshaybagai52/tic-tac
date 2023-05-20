c97a210240e7280cbeeba79421da684a1b34f95a
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-05-20 14:23:49 +0530

    implemented calculate winner

--- vite-project/src/components/Square.jsx
+++ vite-project/src/components/Square.jsx
@@ -13,6 +13,9 @@ const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
     function handleClick(i) {
         const nextSquares = squares.slice();
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+          }
         if(xIsNext) {
             nextSquares[i] = "X";
         } else {
@@ -21,8 +24,17 @@ const Board = () => {
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
     }
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner) {
+        status = "Winner: " + winner;
+        } else {
+            status = "Next Player: " + (xIsNext ? "X" : "O");
+        }
     return (
+        
         <main className='grid grid-cols-1'>
+        <div>{status}</div>
             <section className=''>
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -44,5 +56,25 @@ const Board = () => {
         </main>
     )
 }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
 export default Board
